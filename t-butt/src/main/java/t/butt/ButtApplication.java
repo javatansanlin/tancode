8b7691d89882a3973938bfaccec3d8f57032fab5
Administrator <Administrator@192.168.1.117>
2018-08-31 18:55:28 +0800

    fist commit

---
+++ t-butt/src/main/java/t/butt/ButtApplication.java
@@ -0,0 +1,12 @@
+package t.butt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ButtApplication.class, args);
+    }
+}
