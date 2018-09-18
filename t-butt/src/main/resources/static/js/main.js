ca1923294a4567581d5ace5285912df43f776d1e
javatansanlin <tansanlin.1215>
2018-09-18 22:59:56 +0800

    前端提交

--- t-butt/src/main/resources/static/js/main.js
+++ t-butt/src/main/resources/static/js/main.js
@@ -1,6 +1,13 @@
 function getQueryString(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-    var r = window.location.search.substr(1).match(reg);
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg); //匹配目标参数
     if (r != null) return unescape(r[2]);
-    return null;
+    return null; //返回参数值
 }
+$(function(){
+    var oid  = getQueryString("oid");
+    $("#id").attr("href","index.html?oid="+oid);
+    $("#jfdh").attr("href","jfgoods.html?oid="+oid);
+    $("#me").attr("href","user.html?oid="+oid);
+})
+
